da23fcfa16f7b98a56bceeb9657558febc0ac70c
SergeiBas <sergei.bas06@gmail.com>
2023-04-13 23:38:05 +0200

    one

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,35 @@
+import java.io.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void writeFile() {
+        File file = new File("..//java/test.txt");
+        //try-with-resources
+        try (FileWriter writer = new FileWriter(file))
+        {
+            String text = "Hello, World!";
+            writer.write(text);
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
+    public static void readFileWithInputStream() {
+        try(FileInputStream fileInputStream = new FileInputStream("./files/test.txt")) {
+            System.out.println("File size is bytes: " + fileInputStream.available());
+            byte[] buffer = new byte[fileInputStream.available()];
+            fileInputStream.read(buffer);
+            for (int i = 0; i < buffer.length; i++) {
+                System.out.print((char) buffer[i]);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void main(String[] args) {
+        //writeFile();
+        readFileWithInputStream();
+    }
+
 }
